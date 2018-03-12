c9df16a84d7e78b8b03fa159416dc086953c5e17
eric mercado <ericmercado@macMatrix.local>
2018-03-12 00:06:19 -0600

    Adding interpolation to the product list template

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,5 +6,27 @@ import { Component } from "@angular/core";
 
 })
 export class ProductListComponent {
+        pageTitle: String = 'Product List'
+        products: any[] = [
+            {
+                "productId": 2,
+                "productName": "Garden Cart",
+                "productCode": "GDN-0023",
+                "releaseDate": "March 18, 2016",
+                "description": "15 gallons capacity",
+                "price": 32.99,
+                "starRating": 4.2,
+                "imageUrl": "http://openclipart.org/image/2400px/svg_to_png/58471/garden-cart.png",
 
+            },{
+                "productId": 5,
+                "productName": "Hammer",
+                "productCode": "tbx-0048",
+                "releaseDate": "May 21, 2016",
+                "description": "Curved claw steel hammer",
+                "price":8.9 ,
+                "starRating":4.8 ,
+                "imageUrl": "http://openclipart.org/image/2400px/svg_to_png/193438/freehammer.png",
+            }
+        ]
 }
